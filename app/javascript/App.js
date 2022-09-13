b75ebe918fc7cbe432ff7e1f2d8d6bdcaae9a4aa
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-09-13 12:24:40 -0500

    Add Greeting component that displays a message

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Greeting from './components/Greeting';
 
 export default function App() {
   return (
     <div>
       <h1>My App</h1>
-      <Routes />
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <ul>
+              <li><a href="#/greetings">greet me.</a></li>
+            </ul>
+          )}
+        />
+        <Route path="/greetings" element={<Greeting />} />
+      </Routes>
     </div>
   );
 }
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Greeting() {
+  const message = 'Hello!';
+  return (
+    <div>
+      <h2>{message}</h2>
+    </div>
+  );
+}
