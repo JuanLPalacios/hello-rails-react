a139b6ae47bc433770ab4262bd3c7ceeef472c8a
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-09-13 13:16:09 -0500

    Display the random greeting in Greeting component

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreeting } from '../redux/greetings/greetings';
 
 export default function Greeting() {
-  const message = 'Hello!';
+  const message = useSelector((state) => state.greetings);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getGreeting());
+  }, []);
   return (
     <div>
       <h2>{message}</h2>
